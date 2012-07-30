d0b21d76509532928bd85aa5059392d65d234d05
Jonathan Salis <jtsseneca@hotmail.com>
2012-07-30 14:17:01 -0400

    Redoing my_player require before anything else

--- config/initializers/db.rb
+++ config/initializers/db.rb
@@ -1,4 +1,4 @@
-require_relative File.join("..", "", "my_player.rb")
+require_relative File.join("..", "..", "lib", "models", "my_player.rb")
 
 Dir.glob(File.join('lib', 'models', '*.rb')).each do |file|
   require_relative File.join("..", "..", file)
