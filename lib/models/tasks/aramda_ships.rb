882dbdccac49e2349a9c35375db19385a496beef
Jonathan Salis <jtsseneca@hotmail.com>
2012-08-21 22:15:44 -0400

    Correcting total_cost variable in armada ships

--- lib/models/tasks/aramda_ships.rb
+++ lib/models/tasks/aramda_ships.rb
@@ -8,8 +8,20 @@ class ArmadaShips
     player = MyPlayer.first
     max_fuel = (Functions.get_numeric_variable('MAX_SHIP_FUEL') / 3).to_i
     max_speed = (Functions.get_numeric_variable('MAX_SHIP_SPEED') / 3).to_i
+
+    prospecting, attack, defense, engineering = 40, 200, 200, 40
+
+    total_cost = ((PriceList.ship) +
+      (PriceList.defense * defense) +
+      (PriceList.attack * attack) +
+      (PriceList.prospecting * prospecting) +
+      (PriceList.engineering * engineering) +
+      max_fuel +
+      max_speed
+    ) * num
+
     player.convert_fuel_to_money(total_cost - player.balance) if player.balance < total_cost
-    if ship = MyShip.create_ships_at(num, closest_planet_to_objective, 'armada', 40, 200, 200, 40, 'MINE', planet_to_conquer.id, max_speed, max_fuel).first
+    if ship = MyShip.create_ships_at(num, closest_planet_to_objective, 'armada', prospecting, defense, attack, engineering, 'MINE', planet_to_conquer.id, max_speed, max_fuel).first
       ship.course_control((Functions.distance_between(planet_to_conquer, closest_planet_to_objective) / 2).to_i, nil, planet_to_conquer.location)
     end
   end
