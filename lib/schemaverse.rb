1f97032e0a626798a4b7e4aaea335aaae4515bf6
Jonathan Salis <jtsseneca@hotmail.com>
2012-08-22 13:21:13 -0400

    Creating travellers and armadas when there is not need to stash

--- lib/schemaverse.rb
+++ lib/schemaverse.rb
@@ -68,6 +68,14 @@ class Schemaverse
               @planets_to_create_objects.each do |arr|
                 Resque.enqueue(CreateShipsAtPlanet, arr[0], arr[1], arr[2], arr[3])
               end
+
+              @travellers_to_deploy.each do |travs|
+                Resque.enqueue(TravellingShips, travs[0], travs[1], travs[2])
+              end
+
+              @armadas_to_deploy.each do |armad|
+                Resque.enqueue(ArmadaShips, armad[0], armad[1], armad[2])
+              end
             end
 
             refuel_ships
