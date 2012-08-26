50545d5e87a694ee7e517d89829410050a1f8a3b
Jonathan Salis <jtsseneca@hotmail.com>
2012-08-25 23:23:18 -0400

    Changing travelling ships to be faster when already maxed out

--- lib/models/tasks/aramda_ships.rb
+++ lib/models/tasks/aramda_ships.rb
@@ -3,27 +3,30 @@ class ArmadaShips
 
   def self.perform(start_planet_id, objective_planet_id, num)
 
-    closest_planet_to_objective = Planet.find start_planet_id
-    planet_to_conquer = Planet.find objective_planet_id
-    player = MyPlayer.first
-    max_fuel = (Functions.get_numeric_variable('MAX_SHIP_FUEL') / 3).to_i
-    max_speed = (Functions.get_numeric_variable('MAX_SHIP_SPEED') / 3).to_i
+    if MyShip.count < (2000 - num)
 
-    prospecting, attack, defense, engineering = 480, 0, 0, 0
+      closest_planet_to_objective = Planet.find start_planet_id
+      planet_to_conquer = Planet.find objective_planet_id
+      player = MyPlayer.first
+      max_fuel = (Functions.get_numeric_variable('MAX_SHIP_FUEL') / 3).to_i
+      max_speed = (Functions.get_numeric_variable('MAX_SHIP_SPEED') / 3).to_i
 
-    total_cost = ((PriceList.ship) +
-      (PriceList.defense * defense) +
-      (PriceList.attack * attack) +
-      (PriceList.prospecting * prospecting) +
-      (PriceList.engineering * engineering) +
-      max_fuel +
-      max_speed
-    ) * num
+      prospecting, attack, defense, engineering = 480, 0, 0, 0
 
-    player.convert_fuel_to_money(total_cost - player.balance) if player.balance < total_cost
-    if ships = MyShip.create_ships_at(num, closest_planet_to_objective, 'armada', prospecting, defense, attack, engineering, 'MINE', planet_to_conquer.id, max_speed, max_fuel, 0, [20, 0, 0, 0])
-      ships.each do |ship|
-        ship.course_control((Functions.distance_between(planet_to_conquer, closest_planet_to_objective) / 2).to_i, nil, planet_to_conquer.location)
+      total_cost = ((PriceList.ship) +
+        (PriceList.defense * defense) +
+        (PriceList.attack * attack) +
+        (PriceList.prospecting * prospecting) +
+        (PriceList.engineering * engineering) +
+        max_fuel +
+        max_speed
+      ) * num
+
+      player.convert_fuel_to_money(total_cost - player.balance) if player.balance < total_cost
+      if ships = MyShip.create_ships_at(num, closest_planet_to_objective, 'armada', prospecting, defense, attack, engineering, 'MINE', planet_to_conquer.id, max_speed, max_fuel, 0, [20, 0, 0, 0])
+        ships.each do |ship|
+          ship.course_control((Functions.distance_between(planet_to_conquer, closest_planet_to_objective) / 2).to_i, nil, planet_to_conquer.location)
+        end
       end
     end
   end
--- lib/models/tasks/travelling_ships.rb
+++ lib/models/tasks/travelling_ships.rb
@@ -3,12 +3,14 @@ class TravellingShips
   @queue = :moving
 
   def self.perform(expand_planet_id, max_fuel, max_speed)
-    expand_planet = Planet.find(expand_planet_id)
-    explorer_object = expand_planet.closest_planets(1).my_planets.first
-    player = MyPlayer.first
-    if ship = MyShip.create_ships_at(1, explorer_object, 'traveller', 80, 150, 150, 100, 'MINE', expand_planet.id, max_speed, max_fuel).first
-      ship.course_control((Functions.distance_between(explorer_object, expand_planet) / 2).to_i, nil, expand_planet.location)
-      ship.refuel_ship
+    if MyShip.count < 2000
+      expand_planet = Planet.find(expand_planet_id)
+      explorer_object = expand_planet.closest_planets(1).my_planets.first
+      player = MyPlayer.first
+      if ship = MyShip.create_ships_at(1, explorer_object, 'traveller', 80, 150, 150, 100, 'MINE', expand_planet.id, max_speed, max_fuel).first
+        ship.course_control((Functions.distance_between(explorer_object, expand_planet) / 2).to_i, nil, expand_planet.location)
+        ship.refuel_ship
+      end
     end
   end
 end
